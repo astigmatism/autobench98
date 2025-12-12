8b85edeb951f12124e51ff48b35268ded34e4cd1
Eric Glomstad <eric@crazyerics.com>
2025-12-11 20:02:24 -0800

    +

--- run-in-tmux.sh
+++ run-in-tmux.sh
@@ -14,13 +14,5 @@ if "${TMUX_CMD[@]}" has-session -t "$SESSION_NAME" 2>/dev/null; then
   exit 0
 fi
 
-# Create a new detached tmux session and run the app in a shell wrapper
-# so that if it exits, we can still see the output and exit code.
-"${TMUX_CMD[@]}" new-session -d -s "$SESSION_NAME" \
-  "cd /home/astigmatism/autobench98 && \
-   /home/astigmatism/autobench98/linux-start.sh --no-env-ask; \
-   EXIT_CODE=\$?; \
-   echo; \
-   echo \"linux-start.sh exited with code \$EXIT_CODE\"; \
-   echo \"Press Enter to close this window...\"; \
-   read"
+# Create a new detached tmux session and run the app
+"${TMUX_CMD[@]}" new-session -d -s "$SESSION_NAME" "./start-linux.sh --no-env-ask"
